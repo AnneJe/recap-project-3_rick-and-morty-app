2c9e54d98fe33349504c8ba79680a8dbf7698ff4
Vlatko Juric <juricv78gmail.com>
2023-10-13 16:27:44 +0200

    added more characters with fetched data from api

--- components/card/card.js
+++ components/card/card.js
@@ -1,28 +1,28 @@
-export default function createCharacterCard() {
+export default function createCharacterCard(character) {
   const characterCard = document.createElement("li");
   characterCard.innerHTML = `
     <li class="card">
     <div class="card__image-container">
       <img
         class="card__image"
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        alt="Rick Sanchez"
+        src=${character.image}
+        alt=${character.name}
       />
       <div class="card__image-gradient"></div>
     </div>
     <div class="card__content">
-      <h2 class="card__title">Rick Sanchez</h2>
+      <h2 class="card__title">${character.name}</h2>
       <dl class="card__info">
         <dt class="card__info-title">Status</dt>
-        <dd class="card__info-description">Alive</dd>
+        <dd class="card__info-description">${character.status}</dd>
         <dt class="card__info-title">Type</dt>
-        <dd class="card__info-description"></dd>
-        <dt class="card__info-title">Occurrences</dt>
-        <dd class="card__info-description">51</dd>
+        <dd class="card__info-description">${character.type}</dd>
+        <dt class="card__info-title">Occurences</dt>
+        <dd class="card__info-description">${character.episode.length}</dd>
       </dl>
     </div>
   </li>
   `;
   return characterCard;
 }
-// ${cardTitle}
+//
